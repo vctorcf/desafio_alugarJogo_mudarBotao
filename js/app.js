42b9d0c0e7fc559afc07d72d196693d7076332c9
Victor Chagas Franca <vctorcf@gmail.com>
2025-03-10 13:45:34 -0300

    Perguntando se o usuario realmente quer devolver o jogo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+function confirmarDevolucao(jogo){
+    confirmar = prompt(`Tem certeza que gostaria de devolver o jogo ${jogo}(1 - SIM, 2 - NÃO)?`);
+    if (confirmar == 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function alterarStatus(id){
     let jogo = document.getElementById(`game-${id}`);
     let ImagemDoJogo = jogo.querySelector('.dashboard__item__img');//coloca-se um . antes do query selecionado quando você quiser dizer que é uma classe
@@ -6,16 +15,19 @@ function alterarStatus(id){
 
     console.log(`O usuário clicou no botão do jogo ${jogoNome.innerHTML}.`);
 
+    
+
     if (ImagemDoJogo.classList.contains("dashboard__item__img--rented")){
-        ImagemDoJogo.classList.remove("dashboard__item__img--rented");
+        let confirma = confirmarDevolucao(jogoNome.innerHTML);
+        if (confirma==true){
+            ImagemDoJogo.classList.remove("dashboard__item__img--rented");
+            botaoEstado.classList.remove("dashboard__item__button--return");
+            botaoEstado.textContent = "Alugar";
+        } else {
+            return;
+        }
     } else {
         ImagemDoJogo.classList.add("dashboard__item__img--rented");
-    }
-
-    if (botaoEstado.classList.contains("dashboard__item__button--return")){
-        botaoEstado.classList.remove("dashboard__item__button--return");
-        botaoEstado.textContent = "Alugar";
-    } else {
         botaoEstado.classList.add("dashboard__item__button--return");
         botaoEstado.textContent = "Devolver";
     }
